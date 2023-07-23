c3964857ef33ce64e298a18071c395a1b19981e5
divyuk <div14kes@gmail.com>
2023-07-23 12:17:13 +0530

    fetch data

--- 22-travel-pin/src/App.jsx
+++ 22-travel-pin/src/App.jsx
@@ -6,7 +6,28 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const Base_URL = "http://localhost:8000";
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${Base_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert("There is some error", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
