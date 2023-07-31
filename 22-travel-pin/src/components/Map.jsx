2458a57b9f10023a59c880c5bfbfda4865cacedf
divyuk <div14kes@gmail.com>
2023-07-31 15:21:27 +0530

    leaflet | map

--- 22-travel-pin/src/components/Map.jsx
+++ 22-travel-pin/src/components/Map.jsx
@@ -1,17 +1,34 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import { useState } from "react";
+
 function Map() {
   const navigate = useNavigate();
+
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        Position : {lat} , {lng}
-      </h1>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
