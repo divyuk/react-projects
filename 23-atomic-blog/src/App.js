83d94f81bd8a31be435bfef694bafec1b713ce39
divyuk <div14kes@gmail.com>
2023-07-31 10:54:26 +0530

    Removing unnesccary imports

--- 23-atomic-blog/src/App.js
+++ 23-atomic-blog/src/App.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { usePosts, PostProvider } from "./PostContext";
 
