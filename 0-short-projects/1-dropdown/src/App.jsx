60b02d8b3f7a7cae71cb7334047a45fa1543d847
divyuk <div14kes@gmail.com>
2023-08-14 19:57:31 +0530

    1st project in short react project

---
+++ 0-short-projects/1-dropdown/src/App.jsx
@@ -0,0 +1,11 @@
+import DropZone from "./DropZone";
+
+function App() {
+  return (
+    <div>
+      <DropZone />
+    </div>
+  );
+}
+
+export default App;
---
+++ 0-short-projects/1-dropdown/src/DropZone.jsx
@@ -0,0 +1,21 @@
+function DropZone() {
+  const fruits = [
+    { label: "Apple", value: "🍎", type: ["Kashmiri", "Himalayan"] },
+    { label: "Banana", value: "🍌", type: ["Blue Java", "Mexican"] },
+    {
+      label: "Orange",
+      value: "🍊",
+      type: ["Butwal", "Khasi Santra", "Shrinagar"],
+    },
+  ];
+  return (
+    <main>
+      {/* Left Container */}
+      <div></div>
+      {/* Right Container */}
+      <div></div>
+    </main>
+  );
+}
+
+export default DropZone;
