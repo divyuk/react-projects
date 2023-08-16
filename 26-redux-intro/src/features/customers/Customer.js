74590f4c6f06323440cfae3ba4175a56818cf25c
divyuk <div14kes@gmail.com>
2023-08-16 12:40:31 +0530

    useSelector

--- 26-redux-intro/src/features/customers/Customer.js
+++ 26-redux-intro/src/features/customers/Customer.js
@@ -1,4 +1,10 @@
+import { useSelector } from "react-redux";
+
 function Customer() {
+  // useSelector provides subscription to the store when in the store
+  // any value changes this gets re-render.
+  const customer = useSelector((store) => store.customer.fullName);
+  console.log(customer);
   return <h2>👋 Welcome, %NAME%</h2>;
 }
 
--- 26-redux-intro/src/index.js
+++ 26-redux-intro/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import store from "./store";
@@ -9,6 +10,8 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
