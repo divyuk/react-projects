02187834ed5569a5a90955ebc859c4488225ca4a
divyuk <div14kes@gmail.com>
2023-08-16 13:23:12 +0530

    Handled Dispatch of various transcation

--- 26-redux-intro/src/index.js
+++ 26-redux-intro/src/index.js
@@ -5,8 +5,6 @@ import "./index.css";
 import App from "./App";
 import store from "./store";
 
-store.dispatch({ type: "account/deposit", payload: 233 });
-console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
