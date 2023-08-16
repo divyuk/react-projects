5774255315e3566600de7b6ff8dbae7625c1bc0d
divyuk <div14kes@gmail.com>
2023-08-16 12:51:36 +0530

    Connected the app with redux and implemented some dispatch

--- 26-redux-intro/src/features/customers/Customer.js
+++ 26-redux-intro/src/features/customers/Customer.js
@@ -4,8 +4,7 @@ function Customer() {
   // useSelector provides subscription to the store when in the store
   // any value changes this gets re-render.
   const customer = useSelector((store) => store.customer.fullName);
-  console.log(customer);
-  return <h2>👋 Welcome, %NAME%</h2>;
+  return <h2>👋 Welcome, {customer}</h2>;
 }
 
 export default Customer;
