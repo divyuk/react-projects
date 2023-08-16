57c96c409e65ee17371395f905e60e5145714d2c
divyuk <div14kes@gmail.com>
2023-08-16 16:23:07 +0530

    Redux + Thunks

--- 26-redux-intro/src/features/accounts/accountSlice.js
+++ 26-redux-intro/src/features/accounts/accountSlice.js
@@ -2,12 +2,17 @@ const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
+  isLoading: false,
 };
 //NOt allowed to modify existing state and perform  async operations.
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit": // state_name/event_name'
-      return { ...state, balance: state.balance + action.payload };
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
     case "account/withdraw": // state_name/event_name'
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
@@ -25,14 +30,35 @@ export default function accountReducer(state = initialStateAccount, action) {
         loanPurpose: "",
         balance: state.balance - state.loan,
       };
+    case "account/convertingCurrency":
+      return {
+        ...state,
+        isLoading: true,
+      };
     default:
       return state;
   }
 }
 
 //! Action Creator
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+  // If the currency is differnt we need to make an API call to do
+  // the conversion
+  // Writing a thunk function for dispatching an action...
+  // This function will be called by the redux
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    // API call
+    const host = "api.frankfurter.app";
+    const res = await fetch(
+      `https://${host}/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+    // return action
+    dispatch({ type: "account/deposit", payload: converted });
+  };
 }
 export function withdraw(amount) {
   return { type: "account/withdraw", payload: amount };
--- 26-redux-intro/src/store.js
+++ 26-redux-intro/src/store.js
@@ -1,12 +1,14 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSice";
+import thunk from "redux-thunk";
+
 // ? We need to combine multiple reducers to make it one reducer called rootReducer.
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
