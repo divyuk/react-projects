c951f77a6b81f33eee2a4b5ba28097bddc80659d
divyuk <div14kes@gmail.com>
2023-08-16 11:55:10 +0530

    Customer Reducer

--- 26-redux-intro/src/store.js
+++ 26-redux-intro/src/store.js
@@ -1,13 +1,20 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
+// More state | Customer
+const initialStateCustomer = {
+  fullName: "",
+  nationlID: "",
+  createdAt: "",
+};
+
 //NOt allowed to modify existing state and perform  async operations.
-function reducer(state = initialState, action) {
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit": // state_name/event_name'
       return { ...state, balance: state.balance + action.payload };
@@ -21,7 +28,7 @@ function reducer(state = initialState, action) {
         loanPurpose: action.payload.purpose,
         balance: state.balance + action.payload.amount,
       };
-    case "account/payloan":
+    case "account/payLoan":
       return {
         ...state,
         loan: 0,
@@ -33,14 +40,75 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer);
-store.dispatch({ type: "account/deposit", payload: 500 });
+// For Customer The reducer function
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationlID: action.payload.nationlID,
+        createdAt: action.payload.createdAt,
+      };
+
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+// ? We need to combine multiple reducers to make it one reducer called rootReducer.
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
 });
 
-store.dispatch({ type: "account/payloan" });
+// This is the store for account
+const store = createStore(rootReducer);
 
+// In older codebases this was there.
+const ACCOUNT_DEPOSIT = "account/deposit";
+
+//! Action Creator
+function deposit(amount) {
+  return { type: ACCOUNT_DEPOSIT, payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(100));
+store.dispatch(requestLoan(1000));
+store.dispatch(payLoan());
+// console.log(store.getState());
+
+//! Action Creator for Customer
+
+function createCustomer(fullName, nationlID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationlID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: "account/updateName", payLoad: fullName };
+}
+
+store.dispatch(createCustomer("Divyanshu Kesarwani", "23121331"));
 console.log(store.getState());
