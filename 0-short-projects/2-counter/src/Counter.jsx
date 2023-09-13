7652ebc32cbbfbc76bbf9139804bd86a0c668ee0
divyuk <div14kes@gmail.com>
2023-09-13 15:46:57 +0530

    Starting a app to showcase redux....🚀

--- 0-short-projects/2-counter/src/Counter.jsx
+++ 0-short-projects/2-counter/src/Counter.jsx
@@ -1,25 +1,56 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initalState = {
+  count: 10,
+  push: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "decrement":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case "addvalue":
+      return {
+        ...state,
+        push: action.payload,
+      };
+    case "valueAdded":
+      return {
+        ...state,
+        count: state.count + state.push,
+        push: 0,
+      };
+    default:
+      return initalState;
+  }
+};
 function Counter() {
-  const [count, setCount] = useState(0);
-  const [push, setPush] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initalState);
 
   function handleSubmit(e) {
     e.preventDefault();
-    setCount(count + push);
-    setPush(0);
+    dispatch({ type: "valueAdded", payload: Number(e.target.value) });
+  }
+  function handleChange(e) {
+    const value = Number(e.target.value) || 0;
+    dispatch({ type: "addvalue", payload: value });
   }
   return (
     <main>
-      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
-      <button onClick={() => setCount((prev) => prev - 1)}>Decrement</button>
-      <label>{count}</label>
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      <label>{state.count}</label>
 
       <form onSubmit={handleSubmit}>
-        <input
-          type="number"
-          value={push}
-          onChange={(e) => setPush(Number(e.target.value))}
-        />
+        <input type="number" value={state.push} onChange={handleChange} />
         <button type="submit">Add</button>
       </form>
     </main>
